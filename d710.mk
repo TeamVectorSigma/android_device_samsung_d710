460b8cb11a2ce84683721ac14670acf0d9915823
justlove joyous <justlovejoy@gmail.com>
2012-10-30 18:20:57 -0400

    merge chris41g suaces

--- d710.mk
+++ d710.mk
@@ -14,8 +14,6 @@
 
 LOCAL_PATH := device/samsung/d710
 
-# DEVICE_PACKAGE_OVERLAYS := $(LOCAL_PATH)/overlay
-
 # Init files
 PRODUCT_COPY_FILES := \
     $(LOCAL_PATH)/init.smdk4210.rc:root/init.smdk4210.rc \
@@ -179,5 +177,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 $(call inherit-product, vendor/samsung/d710/d710-common-dupes.mk)
 # Include common makefile
 $(call inherit-product, device/samsung/galaxys2-common/common.mk)
-DEVICE_PACKAGE_OVERLAYS := $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS := \
+    $(LOCAL_PATH)/overlay \
+    device/samsung/galaxys2-common/overlay
 $(call inherit-product-if-exists, vendor/samsung/d710/d710-vendor.mk)
