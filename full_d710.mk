7f7323adfbcbe11ff60d07455b298a68640dab2b
justlove joyous <justlovejoy@gmail.com>
2012-12-26 09:07:11 -0500

    notifyMe0002

--- full_d710.mk
+++ full_d710.mk
@@ -22,6 +22,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/d710/d710.mk)
+$(call inherit-product, vendor/htc/beats/beats.mk)
 
 # Discard inherited values and use our own instead.
 PRODUCT_NAME := full_d710
